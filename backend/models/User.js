2a22a25ccf72b2402cfc530660262894de38ca8c
Riyaz3777 <8019454867srz@gmail.com>
2025-08-30 11:47:52 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,10 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
+  role: { type: String, enum: ["admin", "user"], default: "user" }
 }, { timestamps: true });
-
-// Hash password before save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-// Compare password
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 module.exports = mongoose.model("User", userSchema);
